54798dfa400ccae2f7d2affb7e2b47f11efce214
Vladislav Shurmin <shurmin.2013@gmail.com>
2021-01-28 07:26:34 +0300

    lesson2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 let money = 100000,
-  income = 10000,
-  addExpenses,
-  deposit,
+  income = "mcdonalds",
+  addExpenses = "gas, Utilities, supermarket ",
+  deposit = true,
   mission = 1000000,
-  period = 10;
+  period = 10,
+  budgetDay = money / 30;
 
-alert("дарова");
-console.log("меня видно");
-console.log("коммиты да да");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log("period " + period + " months");
+console.log("The goal is to make " + mission + " USD");
+
+addExpenses = addExpenses.toLowerCase().split(", ");
+console.log(addExpenses);
+console.log(budgetDay);
+console.log(money % 30);
